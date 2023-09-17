fb5ae9cbe013cb41a5141aa7eb8918e544db352b
evan <evan.adam@yahoo.fr>
2023-09-18 00:09:28 +0200

    mariadb connexion is working

--- web/urlShortner.go
+++ web/urlShortner.go
@@ -1,39 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/http"
-	"os"
 )
 
-type ConfigDatabase struct {
-	Port     string `env:"PORT" env-default:"5432"`
-	Host     string `env:"HOST" env-default:"localhost"`
-	Name     string `env:"USERNAME" env-default:"postgres"`
-	User     string `env:"USER" env-default:"user"`
-	Password string `env:"PASSWORD" end-default:"motdepasse"`
+type conf struct {
+	Ipaddress string `env:"WEBIPADDRESS" env-default:"127.0.0.1"`
+	WebPort   string `env:"WEBPORT" env-default:"5432"`
+
+	DBHost     string `env:"MYSQL_HOST" env-default:"127.0.0.1"`
+	DBPort     string `env:"MYSQL_PORT" env-default:"3658"`
+	DBName     string `env:"DBNAME" env-default:"urlShortener"`
+	DBUser     string `env:"MYSQL_USER" env-default:"root"`
+	DBPassword string `env:"MYSQL_PASSWORD" env-default:"PASSFUCK"`
+	DBHandle   *sql.DB
 }
 
-func shortHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg conf) shortHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 
 }
 
-func setupConfig() {
-	var cfg ConfigDatabase
+func setupConfig() conf {
+	var cfg conf
 
-	err := cleanenv.ReadConfig("config.yml", &cfg)
+	err := cleanenv.ReadConfig(".env", &cfg)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+		log.Fatal(err)
 	}
-	fmt.Println(cfg)
-}
-func main() {
-	setupConfig()
-	http.HandleFunc("/", shortHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	temp := mysql.Config{
+		User:                 cfg.DBUser,
+		Passwd:               cfg.DBPassword,
+		Net:                  "tcp",
+		Addr:                 cfg.DBHost + ":" + cfg.DBPort,
+		DBName:               cfg.DBName,
+		AllowNativePasswords: true,
+	}
+	// Get a database handle.
+	var db *sql.DB
+	db, err = sql.Open("mysql", temp.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+	return cfg
+}
+
+func main() {
+	var cfg = setupConfig()
+	http.HandleFunc("/", cfg.shortHandler)
+
+	log.Fatal(http.ListenAndServe(cfg.Ipaddress+":"+cfg.WebPort, nil))
 
 }
